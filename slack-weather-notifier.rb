7f7a8df150fc2b73127362f63454de27ba12995f
Kenichi Hasegawa <kenichi.hasegawa@o-05471-mac.local>
2018-04-09 18:40:46 +0900

    fix

--- slack-weather-notifier.rb
+++ slack-weather-notifier.rb
@@ -1,13 +1,22 @@
 require 'json'
 require 'open-uri'
-require 'slack-incoming-webhooks'
+require 'slack/incoming/webhooks'
 
 uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
 
 res     = JSON.load(open(uri).read)
 title   = res['title']
+text    = res['description']['text']
 link    = res['link']
 weather = res['forecasts'].first
 
-slack = Slack::Incoming::Webhooks.new ENV['https://hooks.slack.com/services/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']
-slack.post "<#{link}|#{weather['date']}の#{title}>は「#{weather['telop']}」です。"
+attachments = [{
+  title: title,
+  title_link: link,
+  text: text,
+  image_url: weather['image']['url'],
+  color: "#7CD197"
+}]
+
+slack = Slack::Incoming::Webhooks.new ENV['WEBHOOK_URL']
+slack.post "#{weather['date']}の#{title}は「#{weather['telop']}」です。", attachments: attachments
